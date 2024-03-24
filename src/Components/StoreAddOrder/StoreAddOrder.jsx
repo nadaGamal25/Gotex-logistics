18f3c182c7473d506d3cb264b9df56fd9ab9fcd1
nada <gnada1221@gmail.com>
2024-03-24 02:30:39 +0200

    p

--- src/Components/StoreAddOrder/StoreAddOrder.jsx
+++ src/Components/StoreAddOrder/StoreAddOrder.jsx
@@ -6,6 +6,57 @@ import BarcodeScanner from '../BarcodeScanner/BarcodeScanner';
 import { useZxing } from "react-zxing";
 
 export default function StoreAddOrder() {
+
+  const [result, setResult] = useState("");
+  // const { ref } = useZxing({
+  //   onDecodeResult(result) {
+  //     setResult(result.getText());
+  //   },
+  // });
+
+  // return (
+  //   <>
+  //   <div className="p-5" id='content'>
+  //     <video ref={ref} />
+  //     <p>
+  //       <span>Last result:</span>
+  //       <span>{result}</span>
+  //     </p>
+  //     </div>
+  //   </>
+  // );
+  const { ref, start, stop } = useZxing({
+    onDecodeResult(result) {
+      setResult(result.getText());
+    },
+  });
+
+  // Function to start the scanner
+  const startScanner = () => {
+    start();
+  };
+
+  // Function to stop the scanner
+  const stopScanner = () => {
+    stop();
+  };
+
+  return (
+    <>
+      <div className="p-5" id='content'>
+        <video ref={ref} />
+        <p>
+          <span>Last result:</span>
+          <span>{result}</span>
+        </p>
+        <button onClick={startScanner}>Start Scanner</button>
+        <button onClick={stopScanner}>Stop Scanner</button>
+      </div>
+    </>
+  );
+}
+
+
   // const [barcode, setBarcode] = useState('');
 
   // const handleDetected = (data) => {
@@ -33,23 +84,3 @@ export default function StoreAddOrder() {
   //     {barcode && <p>Detected Barcode: {barcode}</p>}
   //   </div>
   // );
-
-  const [result, setResult] = useState("");
-  const { ref } = useZxing({
-    onDecodeResult(result) {
-      setResult(result.getText());
-    },
-  });
-
-  return (
-    <>
-    <div className="p-5" id='content'>
-      <video ref={ref} />
-      <p>
-        <span>Last result:</span>
-        <span>{result}</span>
-      </p>
-      </div>
-    </>
-  );
-}
